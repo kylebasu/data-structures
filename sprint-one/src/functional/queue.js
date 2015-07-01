168fc25fc9a757c8c58ee91e54359e6997a1054a
kylebasu <kylebasu@gmail.com>
2015-07-01 16:33:14 -0700

    implement functional & functional-shared stack+queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    length++;
+    storage[length] = value;
   };
 
   someInstance.dequeue = function(){
+    if(length > 0) {
+      length--;      
+    }
+    //{1:b 2:c 3:c}
+    //Hey I want storage[1] = storage[2]
+    for (var i = 0; i <= length; i++){
+      storage[i] = storage[i+1]
+    }
+    return storage[0]
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
