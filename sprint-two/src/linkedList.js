6327513afea7e0a8997740134c63ec8b173053c1
kylebasu <kylebasu@gmail.com>
2015-07-02 22:03:33 -0700

    linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (!list.tail){
+      list.head = new Node(value);
+      list.tail = list.head;
+    }else{
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+
   };
 
   list.removeHead = function(){
+    if(list.head.next === null) {
+      list.head.next = list.tail  
+    }
+    list.head = list.head.next;
+    return list.head.value; 
   };
 
   list.contains = function(target){
+  
+    var checker = function (item){
+      if(item.value === target){
+        return true
+      }
+      if (item.next !== null){
+        return checker(item.next)
+      }
+      return false
+
+    }
+    return checker(list.head); 
+
   };
 
   return list;
